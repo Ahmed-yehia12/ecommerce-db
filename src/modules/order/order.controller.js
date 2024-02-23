c975da21e2254a9199df5be8ad0bd58a2ae5053b
Ahmed-yehia12 <ahmed.yehia.abdulgawad@gmail.com>
2024-02-23 10:33:21 +0200

    stripe order

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -101,7 +101,29 @@ const createCheckOutSession = async(req,res,next)=>{
 
 }
 
+const createOnlinePayment =  (request, response) => {
+    const sig = request.headers['stripe-signature'].toString();
+  
+    let event;
+  
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, "whsec_kc6q3lpjr3184KNlE4TcjV60h1rY9Gob");
+    } catch (err) {
+        return response.status(400).send(`Webhook Error: ${err.message}`);
+    
+    }
+  
+    // Handle the event
+    if(event.type =='checkout.session.completed'){
+        const checkoutSessionCompleted = event.data.object;
+        console.log("create order here...");
 
+
+    }else{
+        console.log(`Unhandled event type ${event.type}`);
+    }
+   
+  }
 
 
 
@@ -109,5 +131,6 @@ export {
     createCashOrder,
     getLoggedUserOrder,
     getAllOrder,
-    createCheckOutSession
+    createCheckOutSession,
+    createOnlinePayment
 }
