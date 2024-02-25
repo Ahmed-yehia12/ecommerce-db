ea804678be62bcf3a1d418f9cbb846be2e3fce6f
Ahmed-yehia12 <ahmed.yehia.abdulgawad@gmail.com>
2024-02-25 07:42:25 +0200

    db

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 
 export const dbConnection =()=>{
-    mongoose.connect("mongodb+srv://ahmedyehia:kt814iNF28xwsTd8@cluster0.h8aqkgl.mongodb.net/EcommerceDB")
+    mongoose.connect(process.env.DATABASE_CONNECTION)
     .then(()=>{console.log("Db is connected..");})
     .catch((err)=>{console.log("database error",err);})
 }
